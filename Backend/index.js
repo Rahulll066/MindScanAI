8eeddef680c5fe043c44642682d4246c3d380475
Rahulll066 <rahulsainadhmail@gmail.com>
2025-09-22 14:06:15 +0530

    Index.js error fixing

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ import express, { json } from "express";
 import cors from "cors";
 import connectDB from "./db.js";
 import authRoutes from "./routes/authRoutes.js";
+import protectedRoutes from "./routes/protectedRoutes.js";
 
 const app = express();
 app.use(cors());
@@ -11,7 +12,12 @@ app.use(json());
 
 connectDB(); // connect to MongoDB
 
+// Public routes
 app.use("/api", authRoutes);
+
+// Protected routes
+app.use("/api", protectedRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
